827a01593b45cc2dc4c2942bc67835f329a56fee
LunarArtemis <ireena51@gmail.com>
2022-11-15 13:43:13 +0700

    change permission page

--- php/foo.php
+++ php/foo.php
@@ -1,13 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="https://unpkg.com/tailwindcss/dist/tailwind.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://unpkg.com/flowbite@1.5.3/dist/flowbite.min.css" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/firebase/9.13.0/firebase-app.js"></script>
+    <script src="https://unpkg.com/flowbite@1.5.3/dist/flowbite.js"></script>
+    <title>Access Denined</title>
 </head>
-<body>
-    <h1 style="color:red;  text-align:center; margin-top: 20%; font-size: 4rem;"><b>you not have permission to access this page<b></h1>
-    <a href="../index.php" style="text-align:center; font-size: 2rem;"><h4>return to main page</h4></a>
+
+<body style="background:#252525">
+    <div class="">
+        <h1 style="color:red;  text-align:center; margin-top: 20%; font-size: 4rem;"><b>You not have permission to access this page<b></h1>
+        <a href="../index.php">
+            <p class="text-white text-center font-md hover:font-2xl hover:text-blue-500">Return to main page</p>
+        </a>
+    </div>
+
 </body>
+
 </html>
