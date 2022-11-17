778e6ccd7589d60a0b4d0426c8753e072d4fb73e
SSSSSS <97426403+historyz29032@users.noreply.github.com>
2022-11-18 01:01:14 +0700

    Add files via upload

--- php/userSubmitReq.php
+++ php/userSubmitReq.php
@@ -15,6 +15,20 @@
     <link rel="icon" type="image/x-icon" href="https://cdn-icons-png.flaticon.com/512/1471/1471262.png" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://unpkg.com/tailwindcss/dist/tailwind.min.css" rel="stylesheet">
+    <link rel="icon" type="image/x-icon" href="https://cdn-icons-png.flaticon.com/512/1471/1471262.png" />
+	<!-- lordicon -->
+	<script src="https://cdn.lordicon.com/qjzruarw.js"></script>
+	<!-- flowbite -->
+	<link rel="stylesheet" href="https://unpkg.com/flowbite@1.5.3/dist/flowbite.min.css" />
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/firebase/9.13.0/firebase-app.js"></script>
+	<script src="https://unpkg.com/flowbite@1.5.3/dist/flowbite.js"></script>
+	<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script> <!-- sweetalert2 -->
+	<!-- Swiper -->
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css" />
+
+	<link rel="stylesheet" href="CSS/App.css">
+	<script src="JS/App.js"></script>
     <title>Upload images</title>
     <style>
         .img-wrap {
@@ -45,6 +59,45 @@
     </style>
 </head>
 <body>
+<nav class="bg-transparent px-2 sm:px-4 py-2.5 dark:bg-transparent fixed w-full z-20 top-0 left-0  ">
+			<div class="container flex flex-wrap justify-between items-center mx-auto">
+				<a href="../index.php" class="flex items-center">
+					<img src="https://cdn-icons-png.flaticon.com/512/1471/1471262.png" class="mr-3 h-6 sm:h-9" alt="NoodleFans Logo">
+					<span class="self-center text-xl font-semibold whitespace-nowrap text-white">NoodleFan Store</span>
+				</a>
+                <div class="flex md:order-2">
+					<!-- open menu -->
+					<button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
+						<span class="sr-only">Open main menu</span>
+						<svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+							<path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path>
+						</svg>
+					</button>
+				</div>
+				<div class="hidden justify-between items-center w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+					<ul class="flex flex-col p-4 mt-4 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:bg-blue-100 lg:bg-transparent">
+						<li>
+							<a href="pricetable.php" class="block py-2 pr-4 pl-3 text-white rounded md:hover:bg-blue-500 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white ">Product</a>
+						</li>
+							<li>
+								<a href="orderCheck.php" class="block py-2 pr-4 pl-3 text-white rounded md:hover:bg-gray-50 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white ">Order</a>
+							</li>
+						<li>
+							<a href="upload.php" class="block py-2 pr-4 pl-3 text-white rounded md:hover:bg-gray-50 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white ">Upload Image</a>
+						</li>
+						<?php if(@$_SESSION['role']=='admin'){ ?>
+						<li>
+							<a href="adminSubmitReq.php" class="block py-2 pr-4 pl-3 text-white rounded md:hover:bg-gray-50 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white ">Admin Submit</a>
+						</li><?php } ?>
+					</ul>
+				</div>
+			</div>
+            <div class="absolute top-0 right-0 mr-5 mt-5">
+            <a href="../index.php" class="transition-all delay-150 duration-0 ease-in-out hover:duration-150 text-white mx-1 p-1 border-solid border-sky-300 border-2 rounded-xl bg-sky-300 hover:bg-sky-700 hover:text-lg hover:p-2 hover:drop-shadow-lg">Back</a>
+            </div>
+		</nav>
+<div class="relative p-10 bg-gray-700">
+</div>
     <div class="container mx-auto">
         <div class="flex flex-col justify-center px-6 my-12">
             <div class="w-full flex">
@@ -95,5 +148,30 @@
             </div>
         </div>
     </div>
+    <footer class="p-4 bg-gray-50 rounded-lg shadow md:px-6 md:py-8 dark:bg-gray-700">
+		<div class="sm:flex sm:items-center sm:justify-between">
+			<a href="#" class="flex items-center mb-4 sm:mb-0">
+				<img src="https://cdn-icons-png.flaticon.com/512/1471/1471262.png" class="mr-3 h-8" alt="Flowbite Logo" />
+				<span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">NoodleFan</span>
+			</a>
+			<ul class="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
+				<li>
+					<a href="#" class="mr-4 hover:underline md:mr-6 ">About</a>
+				</li>
+				<li>
+					<a href="#" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
+				</li>
+				<li>
+					<a href="#" class="mr-4 hover:underline md:mr-6 ">Licensing</a>
+				</li>
+				<li>
+					<a href="#" class="hover:underline">Contact</a>
+				</li>
+			</ul>
+		</div>
+		<hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+		<span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="" class="hover:underline">NoodleFan™</a>. All Rights Reserved.
+		</span>
+	</footer>
 </body>
 </html>
