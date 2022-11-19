ef51aa07233241ea5cb8292b193a13abb01350ff
SSSSSS <97426403+historyz29032@users.noreply.github.com>
2022-11-19 22:25:59 +0700

    Update pricetable.php

--- pricetable.php
+++ pricetable.php
@@ -31,7 +31,7 @@
         $btn_icon = '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="inline-block ml-1 w-4 h-4 text-white xl:inline"><path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" /></svg>';
         $id_btn = 'btn_login';
         if (isset($_SESSION['role'])) {
-            $btn_login = '<a href="logout.php">Sign out</a>';
+            $btn_login = '<a href="./php/logout.php">Sign out</a>';
             $id_btn = 'btn_logout';
             $btn_icon = '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="inline-block ml-1 w-4 h-4 text-white xl:inline"><path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9" /></svg>';
         }
